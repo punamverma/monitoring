fde94e3fcc00f892e6f0be4c5ee47389b3a15072
Punam Verma <punam.verma@nasa.gov>
2024-03-15 11:10:50 -0700

    Add extra script

--- replace_aud.sh
+++ replace_aud.sh
@@ -2,4 +2,4 @@
 
 set -eo pipefail
 
-sed -i "" "s/\(-accepted.*localutm\) \\\\$/\1,$XSV_AUD \\\\/" ./build/dev/startup/core_service.sh
+sed -i "s/\(-accepted.*localutm\) \\\\$/\1,$XSV_AUD \\\\/" ./build/dev/startup/core_service.sh
